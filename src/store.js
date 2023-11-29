f01ed657ff0374059c8d8e879ca550ec605d67a6
valerioperria <valerio.perria@gmail.com>
2023-11-29 14:44:07 +0100

    fix: apiUrl

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { reactive } from "vue";
 
 export const store = reactive({
     films: [],
-    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=a79c48f58b91e5f890bf08cee08c06dd&query",
+    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=a79c48f58b91e5f890bf08cee08c06dd",
     searchText: "",
 })
