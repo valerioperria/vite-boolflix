2827e0f6c93b61287c589faeb35076ce488c075f
valerioperria <valerio.perria@gmail.com>
2023-11-29 19:25:59 +0100

    feat. Milestone_2 - flags

--- src/components/AppFilmCard.vue
+++ src/components/AppFilmCard.vue
@@ -1,18 +1,41 @@
 <script>
 export default {
-  props: {
-    film: Object,
-  },
-};
+    props: {
+        film: Object,
+    },
+    methonds: {
+
+    }
+}
 </script>
 
 <template>
-  <div>
-    <h3>{{ film.title }}</h3>
-    <p>{{ film.original_title }}</p>
-    <p>{{ film.original_language }}</p>
-    <p>{{ film.vote_average }}</p>
-  </div>
+    <div>
+        <h3>{{ film.title }}</h3>
+        <p>{{ film.original_title }}</p>
+
+        <div v-if="film.original_language === 'it'">
+            <img :src="`src/assets/img/${film.original_language}.png`" alt="">
+        </div>
+
+        <div v-else-if="film.original_language === 'en'">
+            <img :src="`src/assets/img/${film.original_language}.png`" alt="">
+        </div>
+
+        <div v-else-if="film.original_language === 'fr'">
+            <img :src="`src/assets/img/${film.original_language}.png`" alt="">
+        </div>
+
+        <div v-else-if="film.original_language === 'es'">
+            <img :src="`src/assets/img/${film.original_language}.png`" alt="">
+        </div>
+
+        <div v-else>
+            <p>{{ film.original_language }}</p>
+        </div>
+
+        <p>{{ film.vote_average }}</p>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
